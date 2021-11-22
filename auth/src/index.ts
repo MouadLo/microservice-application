41c02938d05032dbbbdfc5e7b3a4a65f395e6cdc
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-22 09:30:28 +0100

    setup the client service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,18 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+
+// configure that traffic is being
+// proxy to our application ingress-ingenex
+app.set('trust proxy', true);
+
 app.use(express.json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,8 +36,15 @@ app.use('*', async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be defined');
+	}
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
+
 	try {
-		await mongoose.connect('mongodb://auth-moongo-srv:27017/auth', {});
+		await mongoose.connect(`${process.env.MONGO_URI}`, {});
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error(error);
